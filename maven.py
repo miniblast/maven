4df7f27c0e27b80f3a2cb639298c88f7073b6b4c
Zack P <zack@zacklearns.com>
2017-09-22 16:38:33 -0600

    Create maven.py

---
+++ maven.py
@@ -0,0 +1,48 @@
+from pyparsing import *
+from sys import *
+
+vars = {}
+varn = ""
+varv = ""
+
+file = open(sys.argv[0]).read()
+
+def prin(s,l,t):
+  print(t)
+  
+def var1(s,l,t):
+  varn = t
+  
+def var2(s,l,t):
+  if varn == "":
+    varv = ""
+  else:
+    varv = t
+    vars[varn] = varv
+    varv = ""
+    varn = ""
+    
+def getvar(s,l,t):
+  if vars[t]:
+    return vars[t]
+  else:
+    print("Variable not found.")
+  
+
+num = Word(nums)
+word = Word(alphas)
+pri = Keyword("pri")
+semi = Word(";",max=1)
+equal = Word("=",max=1)
+vars = Word("!",max=1)
+va = Word("var")
+string = Word("string")
+number = Word("number")
+as = Keyword(as)
+
+# Start Parsing
+pr = pri + " " + (word | num).SetParseAction() + semi                 # pri MyString; pri 100;
+dvar = va + " " + word.SetParseAction(var1) + " " + as + (string | number) + equal + word.SetParseAction(var2) + semi
+callv = vars + word.SetParseAction(getvar)
+
+(pr).parseFile(file)
